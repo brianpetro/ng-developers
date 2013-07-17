04d10eb9421db1b08f08be3f93283111739ece26
brianpetro <brian@plexm.com>
2013-07-17 09:20:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ngdev::Application.config.secret_key_base = '46c199a9bac4cc8c0ae456324a114fbf1caf04c984d70e458fb7c09e557f3d2824a86113c387023e40608eb5eb96945b0d774768767bb72ae5c45b60acb8dc35'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ngdev::Application.config.session_store :cookie_store, key: '_ngdev_session'
